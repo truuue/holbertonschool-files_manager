3902a557e629a97fdf6672d683a7675ba56fc563
truuue <noahvernhet@gmail.com>
2024-07-31 15:47:04 +0200

    refactor: Update import statements for dbClient and redisClient
    This commit updates the import statements in the image_upload.py, main.js, and worker.js files to improve code consistency. The import statements for dbClient and redisClient have been updated to use single quotes instead of double quotes for consistency with the rest of the codebase. This change ensures that the correct modules are imported and improves the overall code readability.

--- worker.js
+++ worker.js
@@ -1,43 +1,32 @@
-import { ObjectId } from 'mongodb';
-import Queue from 'bull';
-import fs from 'fs';
-import imageThumbnail from 'image-thumbnail';
-import dbClient from './utils/db';
+import { writeFileSync } from 'fs';
+import DBClient from './utils/db';
 
-const queue = new Queue('fileQueue');
+const Queue = require('bull');
+const imageThumbnail = require('image-thumbnail');
 
-queue.process(async (job, done) => {
-  try {
-    // data verifications
-    const { userId, fileId } = job.data;
-    if (!fileId) throw new Error('Missing fileId');
-    if (!userId) throw new Error('Missing userId');
+const fileQueue = new Queue('fileQueue');
+const thumbnailSizes = [500, 250, 100];
 
-    const filesCollection = await dbClient.db.collection('files');
-    const image = await filesCollection.findOne(
-      {
-        userId: ObjectId(userId),
-        _id: ObjectId(fileId),
-      },
-    );
-    if (!image) throw new Error('File not found');
-
-    // Generate Thumbnail
-    try {
-      const processedImage100 = await imageThumbnail(image.localPath, { width: 100 });
-      const newPath100 = `${image.localPath}_100`;
-      fs.writeFileSync(newPath100, processedImage100);
-      const processedImage250 = await imageThumbnail(image.localPath, { width: 250 });
-      const newPath250 = `${image.localPath}_250`;
-      fs.writeFileSync(newPath250, processedImage250);
-      const processedImage500 = await imageThumbnail(image.localPath, { width: 500 });
-      const newPath500 = `${image.localPath}_500`;
-      fs.writeFileSync(newPath500, processedImage500);
-    } catch (err) {
-      throw new Error(err);
-    }
-    done();
-  } catch (error) {
-    console.error(error);
+fileQueue.process(async (job, done) => {
+  if (!job.data.fileId) {
+    done(Error('Missing fileId'));
   }
+
+  if (!job.data.userId) {
+    done(Error('Missing userId'));
+  }
+
+  const fileInfo = await DBClient.findFileById(job.data.fileId, job.data.userId);
+  if (!fileInfo) {
+    done(Error('File not found'));
+  }
+
+  const promises = thumbnailSizes.map((width) => imageThumbnail(fileInfo.localPath, { width })
+    .then((buffer) => {
+      writeFileSync(`${fileInfo.localPath}_${width}`, buffer);
+    }));
+
+  await Promise.all(promises);
+
+  done();
 });
