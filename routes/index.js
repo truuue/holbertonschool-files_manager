ba00b80a9fbb6a7c21cae205723efb8ae0d12699
truuue <noahvernhet@gmail.com>
2024-07-31 11:34:37 +0200

    refactor: Update routes/index.js for user authentication

--- routes/index.js
+++ routes/index.js
@@ -8,33 +8,16 @@ const router = Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-
 router.post('/users', UsersController.postNew);
-router.get('/users/me', AuthController.verifyToken, UsersController.getMe);
-
 router.get('/connect', AuthController.getConnect);
-router.get('/disconnect', AuthController.verifyToken, AuthController.getDisconnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
 
-router.post('/files', AuthController.verifyToken, FilesController.postUpload);
+router.post('/files', FilesController.postUpload);
 router.get('/files/:id', FilesController.getShow);
 router.get('/files', FilesController.getIndex);
-router.put('/files/:id/publish', AuthController.verifyToken, FilesController.putPublish);
-router.put('/files/:id/unpublish', AuthController.verifyToken, FilesController.putUnpublish);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+
 router.get('/files/:id/data', FilesController.getFile);
-
-AuthController.verifyToken = (req, res, next) => {
-  const token = req.headers['x-access-token'];
-  if (!token) {
-    return res.status(401).send({ error: 'No token provided.' });
-  }
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.userId = decoded.id;
-    next();
-  } catch (err) {
-    return res.status(401).send({ error: 'Failed to authenticate token.' });
-  }
-};
-
 export default router;
