591f8aa0e4af96247ea6e30ace0cd81ebef30b32
truuue <noahvernhet@gmail.com>
2024-07-31 14:48:18 +0200

    refactor: Update import statements for dbClient and redisClient
    This commit updates the import statements in the controllers to use the correct file extensions for dbClient and redisClient. The extensions have been changed from .js to .mjs to match the actual file names. This change ensures that the correct modules are imported and improves the overall code consistency.

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,7 +2,6 @@ import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
 class AppController {
-  // Méthode pour obtenir le statut de santé des services Redis et MongoDB
   static async getStatus(req, res) {
     const redisStatus = redisClient.isAlive();
     const dbStatus = dbClient.isAlive();
@@ -10,7 +9,6 @@ class AppController {
     res.status(200).json({ redis: redisStatus, db: dbStatus });
   }
 
-  // Méthode pour obtenir des statistiques sur les utilisateurs et les fichiers
   static async getStats(req, res) {
     const users = await dbClient.nbUsers();
     const files = await dbClient.nbFiles();
--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,19 +1,49 @@
-import dbClient from '../utils/db';
-import redisClient from '../utils/redis';
+import sha1 from 'sha1';
+import { v4 as uuidv4 } from 'uuid';
+import dbClient from '../utils/db.mjs';
+import redisClient from '../utils/redis.mjs';
 
-class AppController {
-  static async getStatus(req, res) {
-    const redisStatus = redisClient.isAlive();
-    const dbStatus = dbClient.isAlive();
+const AuthController = {
+  getConnect: async (req, res) => {
+    const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
+    if (!b64auth) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
 
-    res.status(200).json({ redis: redisStatus, db: dbStatus });
-  }
+    const auth = Buffer.from(b64auth, 'base64').toString('utf-8');
+    const email = auth.split(':')[0] || '';
+    const password = auth.split(':')[1];
+    if (!password || !email) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    const hashedPassword = sha1(password);
 
-  static async getStats(req, res) {
-    const users = await dbClient.nbUsers();
-    const files = await dbClient.nbFiles();
+    const usersCollection = dbClient.db.collection('users');
+    const user = await usersCollection.findOne({ email, password: hashedPassword });
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
 
-    res.status(200).json({ users, files });
-  }
-}
-export default AppController;
+    const token = uuidv4();
+    const key = `auth_${token}`;
+    const value = user._id.toString();
+    const duration = 24 * 60 * 60;
+    await redisClient.set(key, value, duration);
+    return res.status(200).json({ token });
+  },
+
+  getDisconnect: async (req, res) => {
+    const token = req.headers['x-token'];
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    await redisClient.del(`auth_${token}`);
+    return res.status(204).end();
+  },
+};
+
+export default AuthController;
