3b4e7a6ba967208698e64c03940e2ceb9224e74d
truuue <noahvernhet@gmail.com>
2024-07-31 14:48:03 +0200

    refactor: Update import statements for dbClient and redisClient
    This commit updates the import statements in the controllers to use the correct file extensions for dbClient and redisClient. The extensions have been changed from .js to .mjs to match the actual file names. This change ensures that the correct modules are imported and improves the overall code consistency.

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,31 +1,21 @@
-import dbClient from '../utils/db.mjs';
-import redisClient from '../utils/redis.mjs';
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
-const AppController = {
-  getStatus: async (req, res) => {
-    try {
-      const dbStatus = await dbClient.isAlive();
-      const redisStatus = await redisClient.isAlive();
-      if (dbStatus && redisStatus) {
-        res.status(200).json({ redis: redisStatus, db: dbStatus });
-      } else {
-        res.status(503).json({ redis: redisStatus, db: dbStatus });
-      }
-    } catch (error) {
-      console.log(error);
-      res.status(400).end();
-    }
-  },
-  getStats: async (req, res) => {
-    try {
-      const nbUsers = await dbClient.users.countDocuments();
-      const nbFiles = await dbClient.files.countDocuments();
-      res.status(200).json({ users: nbUsers, files: nbFiles });
-    } catch (error) {
-      console.log(error);
-      res.status(400).end();
-    }
-  },
-};
+class AppController {
+  // Méthode pour obtenir le statut de santé des services Redis et MongoDB
+  static async getStatus(req, res) {
+    const redisStatus = redisClient.isAlive();
+    const dbStatus = dbClient.isAlive();
 
+    res.status(200).json({ redis: redisStatus, db: dbStatus });
+  }
+
+  // Méthode pour obtenir des statistiques sur les utilisateurs et les fichiers
+  static async getStats(req, res) {
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+
+    res.status(200).json({ users, files });
+  }
+}
 export default AppController;
