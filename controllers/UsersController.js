28b681b9ced807a42ef53cc8c9c48d8b321dc24f
truuue <noahvernhet@gmail.com>
2024-07-31 11:44:27 +0200

    refactor: Improve user authentication in AuthController

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -5,60 +5,46 @@ import redisClient from '../utils/redis';
 
 const UsersController = {
   postNew: async (req, res) => {
-    try {
-      const { email, password } = req.body;
+    const { email, password } = req.body;
 
-      if (!email) {
-        return res.status(400).json({ error: 'Missing email' });
-      }
-
-      if (!password) {
-        return res.status(400).json({ error: 'Missing password' });
-      }
-
-      const usersCollection = dbClient.db.collection('users');
-      const user = await usersCollection.findOne({ email });
-      if (user) {
-        return res.status(400).json({ error: 'Already exist' });
-      }
-
-      const addNewUser = await usersCollection.insertOne({
-        email,
-        password: sha1(password),
-      });
-
-      return res.status(201).json({ email, id: addNewUser.insertedId });
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ error: 'Internal server error' });
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
     }
+
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    const usersCollection = dbClient.db.collection('users');
+    const user = await usersCollection.findOne({ email });
+    if (user) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+
+    const addNewUser = await usersCollection.insertOne({
+      email,
+      password: sha1(password),
+    });
+
+    return res.status(201).json({ email, id: addNewUser.insertedId });
   },
 
   getMe: async (req, res) => {
-    try {
-      const token = req.headers['x-token'];
-      if (!token) {
-        return res.status(401).json({ error: 'Unauthorized: Missing token' });
-      }
-
-      const userId = await redisClient.get(`auth_${token}`);
-      if (!userId) {
-        return res.status(401).json({ error: 'Unauthorized: Invalid token' });
-      }
-
-      const _id = new ObjectId(userId);
-      const usersCollection = dbClient.db.collection('users');
-      const user = await usersCollection.findOne({ _id });
-
-      if (!user) {
-        return res.status(401).json({ error: 'Unauthorized: User not found' });
-      }
-
-      return res.status(200).json({ id: user._id.toString(), email: user.email });
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ error: 'Internal server error' });
+    const token = req.headers['x-token'];
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
     }
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    const _id = new ObjectId(userId);
+    const usersCollection = dbClient.db.collection('users');
+    const user = await usersCollection.findOne({ _id });
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    return res.status(200).json({ id: user._id.toString(), email: user.email });
   },
 };
 
